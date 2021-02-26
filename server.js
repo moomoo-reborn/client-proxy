7ec189472fa28c53ce8d60bd8d30ca01a2118d7c
Meow <50887230+itzTheMeow@users.noreply.github.com>
2021-02-26 18:32:18 -0500

    init

---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const axios = require("axios");
+const app = express();
+
+let baseURL = "http://panel.themeow.ml:6789";
+
+app.get("/", (req, res) => {
+  axios
+    .get(baseURL)
+    .then((content) => {
+      res.status(content.status).send(content.data);
+    })
+    .catch((e) => {
+      res.status(e.response.status).send(e.response.data);
+    });
+});
+
+app.get("*", (req, res) => {
+  let site = baseURL + req.originalUrl;
+  res.redirect(site);
+});
+
+const listener = app.listen(process.env.PORT, () => {
+  console.log("Your app is listening on port " + listener.address().port);
+});
